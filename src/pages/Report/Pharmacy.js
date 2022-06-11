adf528604a4a8a695e9f76ac549d03d0c72cb58e
malikmukhtar <malikmukhtar63@gmail.com>
2022-06-11 16:56:53 +0100

    Report - Lab and Pharmacy reports

--- src/pages/Report/Pharmacy.js
+++ src/pages/Report/Pharmacy.js
@@ -4,6 +4,10 @@ import moment from 'moment';
 import Pagination from 'antd/lib/pagination';
 import TableLoading from '../../components/TableLoading';
 import { paginate } from '../../services/constants';
+import DatePicker from 'antd/lib/date-picker';
+import waiting from '../../assets/images/waiting.gif';
+
+const { RangePicker } = DatePicker;
 
 const Pharmacy = () => {
 	const [drugTransactions, setDrugTransactions] = useState([]);
@@ -16,22 +20,40 @@ const Pharmacy = () => {
 
 	const [linkPharm, setLinkPharm] = useState(true);
 	const [linkDen, setLinkDen] = useState(false);
+	const [startDate, setStartDate] = useState('');
+	const [endDate, setEndDate] = useState('');
+	const [searchValue, setSearchValue] = useState('');
+	// const [status, setStatus] = useState('');
+	const [filtering, setFiltering] = useState(false);
 
-	useEffect(() => {
+	const dateChange = e => {
+		const date = e.map(d => {
+			return moment(d._d).format('YYYY-MM-DD');
+		});
+
+		setStartDate(date[0]);
+		setEndDate(date[1]);
+	};
+
+	const doFilter = e => {
+		if (e) e.preventDefault();
+		setFiltering(true);
 		fetchDrugTransactions();
-	}, [drugTransactions.length]);
+		setFiltering(false);
+	};
 
 	const fetchDrugTransactions = async page => {
 		try {
 			let p = page || 1;
-			let pid = '';
-			let startDate = '';
-			let endDate = '';
-			let service_id = '2';
-			let status = '';
+			// let pid = '';
+			// let startDate = startDate || '';
+			// let endDate = endDate || '';
+			// let service_id = '2';
+			// let status = '';
 			setLoading(true);
-			const url = `transactions?page=${p}&limit=15&patient_id=${pid}&startDate=${startDate}&endDate=${endDate}&service_id=${service_id}&status=${status}`;
+			const url = `transactions/search?bill_source=drugs&page=${p}&limit=10&term=${searchValue}&startDate=${startDate}&endDate=${endDate}`;
 			const rs = await request(url, 'GET', true);
+			console.log('malik', rs);
 			const { result, ...meta } = rs;
 			setDrugTransactions(result);
 			setMeta(meta);
@@ -41,6 +63,10 @@ const Pharmacy = () => {
 			setLoading(false);
 		}
 	};
+
+	useEffect(() => {
+		fetchDrugTransactions();
+	}, [drugTransactions.length]);
 
 	useEffect(() => {
 		fetchDrugDispensations();
@@ -111,6 +137,42 @@ const Pharmacy = () => {
 						</ul>
 					</div>
 				</div>
+				<div className="element-box m-0 mb-4 p-3">
+					<form className="row">
+						<div className="form-group col-md-3">
+							<label>From - To</label>
+							<RangePicker onChange={e => dateChange(e)} />
+						</div>
+						<div className="form-group col-md-3">
+							<label className="mr-2 " htmlFor="id">
+								Search
+							</label>
+							<input
+								style={{ height: '32px' }}
+								id="search"
+								className="form-control"
+								name="search"
+								onChange={e => setSearchValue(e.target.value)}
+								placeholder="search name,patient id, drug, amount, qty"
+							/>
+						</div>
+						<div className="form-group col mt-4">
+							<div
+								className="btn btn-sm btn-primary btn-upper text-white filter-btn"
+								onClick={doFilter}
+							>
+								<i className="os-icon os-icon-ui-37" />
+								<span>
+									{filtering ? (
+										<img src={waiting} alt="submitting" />
+									) : (
+										'Filter'
+									)}
+								</span>
+							</div>
+						</div>
+					</form>
+				</div>
 				<div className="row">
 					<div className="col-sm-8 col-lg-9 col-xl-6 col-xxl-12">
 						<div className="element-box">
@@ -131,7 +193,7 @@ const Pharmacy = () => {
 																			}`}
 																			onClick={handleLinkPharm}
 																		>
-																			<span>Pharmacy</span>
+																			<a>Pharmacy</a>
 																		</div>
 																	</li>
 																	<li className="nav-item">
@@ -142,21 +204,13 @@ const Pharmacy = () => {
 																			}`}
 																			onClick={handleLinkDen}
 																		>
-																			<span>Dispensed</span>
+																			<a>Dispensed</a>
 																		</div>
 																	</li>
 																</ul>
 															</div>
 														</div>
 													</div>
-												</div>
-												<div className="col-sm-4">
-													<form className="form-inline justify-content-sm-end">
-														<input
-															className="form-control form-control-sm rounded bright"
-															placeholder="Search"
-														/>
-													</form>
 												</div>
 											</div>
 										</div>
@@ -174,20 +228,20 @@ const Pharmacy = () => {
 																		<th>Patient ID</th>
 																		<th>Request Date</th>
 																		<th className="text-center">Fill Date</th>
-																		<th className="text-right">Drug Item</th>
+																		<th className="text-center">Drug Item</th>
 																		<th className="text-right">Amount</th>
 																		<th className="text-right">Quantity</th>
 																	</tr>
 																</thead>
 																<tbody>
-																	{drugTransactions.map(
+																	{drugTransactions?.map(
 																		(transaction, index) => (
 																			<tr key={index}>
 																				<td>
 																					{transaction.patient.surname}{' '}
 																					{transaction.patient.other_names}
 																				</td>
-																				<td>{transaction.patient_id}</td>
+																				<td>{transaction.patient.id}</td>
 																				<td>
 																					{moment(transaction.createdAt).format(
 																						'DD-MM-YYYY h:mm a'
@@ -199,18 +253,14 @@ const Pharmacy = () => {
 																							.filled_at
 																					).format('DD-MM-YYYY h:mm a')}
 																				</td>
-																				<td className="text-right">
+																				<td className="text-left">
 																					{
-																						transaction.patientRequestItem.drug
-																							.name
+																						transaction.patientRequestItem
+																							.drugGeneric.name
 																					}
 																				</td>
 																				<td className="text-right">
-																					&#x20A6;{' '}
-																					{
-																						transaction.patientRequestItem
-																							.drugBatch.unitPrice
-																					}
+																					&#x20A6; {transaction.amount * -1}
 																				</td>
 																				<td className="text-right">
 																					{
@@ -230,7 +280,7 @@ const Pharmacy = () => {
 																<Pagination
 																	current={parseInt(meta.currentPage, 10)}
 																	pageSize={parseInt(meta.itemsPerPage, 10)}
-																	total={parseInt(meta.totalPages, 10)}
+																	total={parseInt(meta.totalItems, 10)}
 																	showTotal={total => `Total ${total} items`}
 																	itemRender={itemRender}
 																	onChange={onNavigatePage}
